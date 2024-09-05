ab49f0f2b9896df3361504458d9d241dd213f380
Galaxy-104 <js_park95@naver.com>
2024-09-05 23:26:54 +0900

    feat : add inquire individual data

--- schedules.js
+++ schedules.js
@@ -1,4 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const map = new Map([
+    [1, {
+        title : "오리엔테이션",
+        date : "2024-08-05",
+        time : "09:00",
+        place : "online",
+        categoty : "lecture"
+    }],
+    [2, {
+        title : "온라인 특강",
+        date : "2024-08-27",
+        time : "19:00",
+        place : "online",
+        categoty : "lecture"
+    }],
+    [3, {
+        title : "팀 미팅",
+        date : "2024-08-30",
+        time : "20:00",
+        place : "online",
+        categoty : "project"
+    }],
+]);
+
+// 개별 데이터 조회
+app.get('/schedules/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const schedule = map.get(id);
+
+    if (schedule == undefined) {
+        res.json({ message : "존재하지 않는 데이터입니다." })
+    } else {
+        res.json(schedule);
+    }
+})
 
 app.listen(3002);
