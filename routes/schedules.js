8e2a69a2e76f19c3d7481e8a7150674ccd513091
Galaxy-104 <js_park95@naver.com>
2024-10-01 18:14:26 +0900

    refactor : Remove map code

--- routes/schedules.js
+++ routes/schedules.js
@@ -5,30 +5,6 @@ router.use(express.json());
 
 const conn = require('../mariadb');
 
-const map = new Map([
-    [1, {
-        title : "오리엔테이션",
-        date : "2024-08-05",
-        time : "09:00",
-        place : "online",
-        categoty : "lecture"
-    }],
-    [2, {
-        title : "온라인 특강",
-        date : "2024-08-27",
-        time : "19:00",
-        place : "online",
-        categoty : "lecture"
-    }],
-    [3, {
-        title : "팀 미팅",
-        date : "2024-08-30",
-        time : "20:00",
-        place : "online",
-        categoty : "project"
-    }],
-]);
-
 // 전체 데이터 조회
 router.get('/', (req, res) => {
 
